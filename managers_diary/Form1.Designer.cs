9264e739748e4066a37d9da2f9fcf5919cf93625
EdvKlaus <131393806+EdvKlaus@users.noreply.github.com>
2023-11-18 20:41:34 +0300

    Add files via upload

---
+++ managers_diary/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace managers_diary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.generate_file_button = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.taskColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(291, 301);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(250, 24);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Выбрать задание";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(314, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(209, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Выбрать время на выполнение";
+            // 
+            // generate_file_button
+            // 
+            this.generate_file_button.Location = new System.Drawing.Point(317, 369);
+            this.generate_file_button.Name = "generate_file_button";
+            this.generate_file_button.Size = new System.Drawing.Size(206, 61);
+            this.generate_file_button.TabIndex = 4;
+            this.generate_file_button.Text = "Создать файл с заданием";
+            this.generate_file_button.UseVisualStyleBackColor = true;
+            this.generate_file_button.Click += new System.EventHandler(this.generate_file_button_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.taskColumn});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(853, 250);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // taskColumn
+            // 
+            this.taskColumn.HeaderText = "Задания";
+            this.taskColumn.MinimumWidth = 6;
+            this.taskColumn.Name = "taskColumn";
+            this.taskColumn.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 505);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.generate_file_button);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button generate_file_button;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn taskColumn;
+    }
+}
+
---
+++ managers_diary/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace managers_diary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string task = "Задание не выбрано";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            foreach (var line in File.ReadAllLines(@"D:\ComBox1.txt"))
+            {
+                var array = line.Split();
+                dataGridView1.Rows.Add(array);
+            }
+
+            StreamReader dataCB = new StreamReader(@"D:\ComBox2.txt");
+            string xq = dataCB.ReadToEnd();
+            string[] yq = xq.Split('\n');
+            foreach (string sq in yq)
+            {
+                comboBox2.Items.Add(sq);
+            }
+            
+        }
+
+        private void generate_file_button_Click(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+
+            string fileStr = "Текущая дата: " + dateTime.ToString() + "\n"+ "Задание: " + task + "\n"+ "Время на выполнение: " + comboBox2.SelectedItem.ToString();
+
+            saveFileDialog1.FileName = @"exercise.txt";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var writer = new StreamWriter(
+                    saveFileDialog1.FileName, false, Encoding.GetEncoding(1251));
+
+                    writer.Write(fileStr);
+                    writer.Close();
+                }
+                catch (Exception Ситуация)
+                {
+                    MessageBox.Show(Ситуация.Message,
+                        "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                task = Convert.ToString(dataGridView1[0, e.RowIndex].Value);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error:", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
