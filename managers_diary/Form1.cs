996e8c8b25ae5b7675335c77e5c34af746a68307
EdvKlaus <131393806+EdvKlaus@users.noreply.github.com>
2024-01-21 22:13:14 +0300

    Update Form1.cs

--- managers_diary/Form1.cs
+++ managers_diary/Form1.cs
@@ -14,13 +14,14 @@ namespace managers_diary
         string task = "Задание не выбрано";
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            //Заполнение таблицы для выбора задания
             foreach (var line in File.ReadAllLines(@"D:\ComBox1.txt"))
             {
                 var array = line.Split('\n');
                 dataGridView1.Rows.Add(array);
             }
 
+            //Заполнение ComboBox-а отвечающего за выбор времени выполнения
             StreamReader dataCB = new StreamReader(@"D:\ComBox2.txt");
             string xq = dataCB.ReadToEnd();
             string[] yq = xq.Split('\n');
@@ -31,32 +32,41 @@ namespace managers_diary
             
         }
 
+        //Метод отвечающий за создание файла
         private void generate_file_button_Click(object sender, EventArgs e)
         {
-            DateTime dateTime = DateTime.Now;
-
-            string fileStr = "Текущая дата: " + dateTime.ToString() + "\n"+ "Задание: " + task + "\n"+ "Время на выполнение: " + comboBox2.SelectedItem.ToString();
-
-            saveFileDialog1.FileName = @"exercise.txt";
-            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            if (comboBox2.SelectedItem == null) {
+                MessageBox.Show("Выберите время на выполнение", "Ошибка");
+                
+            }
+            else
             {
-                try
-                {
-                    var writer = new StreamWriter(
-                    saveFileDialog1.FileName, false, Encoding.GetEncoding(1251));
+                //Создание сообщения для записи в файл
+                DateTime dateTime = DateTime.Now;
 
-                    writer.Write(fileStr);
-                    writer.Close();
-                }
-                catch (Exception Ситуация)
+                string fileStr = "Текущая дата: " + dateTime.ToString() + "\n" + "Задание: " + task + "\n" + "Время на выполнение: " + comboBox2.SelectedItem.ToString();
+
+                saveFileDialog1.FileName = @"exercise.txt";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK) //Открытие диалогового окна для выбора пути сохранения файла
                 {
-                    MessageBox.Show(Ситуация.Message,
-                        "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    try
+                    {//Запись данных в файл
+                        var writer = new StreamWriter(
+                        saveFileDialog1.FileName, false, Encoding.GetEncoding(1251));
+
+                        writer.Write(fileStr);
+                        writer.Close();
+                    }
+                    catch (Exception Ситуация)
+                    {
+                        MessageBox.Show(Ситуация.Message,
+                            "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
                 }
             }
         }
 
-        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e) //Определение выбранной задачи
         {
             try
             {
